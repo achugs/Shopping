9a47692595630750722fb3a66140897f2d692717
Achugs <anna_9400@hotmail.com>
2019-09-10 09:47:31 +0100

    created a database file to hold data needed

---
+++ src/components/database.js
@@ -0,0 +1,28 @@
+import Mix from "../images/Cactus_mix.jpg";
+import Black from "../images/Cactus_black.jpg";
+import Yellow from "../images/Cactus_Yellow.jpg";
+import Red from "../images/Cactus_Red.jpg";
+export const database = [
+  {
+    product: "Column Cactus",
+    price: 10,
+    description:
+      "Column Cactus is a name used to describe any cactus with a tall body,so it encompasses a huge variety of cacti.Growing pot size: 10.5cm diameter, 8cm height.Plant size: Height including pot is between 15-20cm depending on specimen.Excludes decorative pot."
+  }
+];
+
+export const images = [
+  {
+    src: Mix
+  },
+  {
+    src: Black
+  },
+  {
+    src: Yellow
+  },
+
+  {
+    src: Red
+  }
+];
