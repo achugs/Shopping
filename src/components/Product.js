fb59b43d81bc2d02c915970353736aceaffb9317
Achugs <anna_9400@hotmail.com>
2019-09-10 10:23:56 +0100

    quantity is increased onClick, data stored

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,13 +4,17 @@ import { AppContext } from "../Basket-Context";
 
 const Product = () => {
   const [basket, setBasket] = useContext(AppContext);
+  const addToBasket = () => {
+    const item = { product: database[0].product, price: database[0].price };
+    setBasket(currentBasket => [...currentBasket, item]);
+  };
   return (
     <div>
       <p>{database[0].product}</p>
       <p>£{database[0].price}</p>
       <p>{database[0].description}</p>
       <p>Quantity in Basket: {basket.length}</p>
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 };
