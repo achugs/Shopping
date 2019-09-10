7474dc82674be3f15df270b8db7f333ffd96a6eb
Achugs <anna_9400@hotmail.com>
2019-09-10 10:21:46 +0100

    added quanitity in basket

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { database } from "./database";
+import { AppContext } from "../Basket-Context";
 
 const Product = () => {
+  const [basket, setBasket] = useContext(AppContext);
   return (
     <div>
       <p>{database[0].product}</p>
       <p>£{database[0].price}</p>
       <p>{database[0].description}</p>
+      <p>Quantity in Basket: {basket.length}</p>
       <button>Add to Basket</button>
     </div>
   );
