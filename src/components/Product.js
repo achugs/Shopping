05b46b68664093e1b1f95b6d60bd90d9268281af
Achugs <anna_9400@hotmail.com>
2019-09-10 10:48:50 +0100

    image carousel added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
-import { database } from "./database";
+import { database, images } from "./database";
 import { AppContext } from "../Basket-Context";
+import { Gallery, GalleryImage } from "react-gesture-gallery";
 
 const Product = () => {
   const [basket, setBasket] = useContext(AppContext);
+  const [index, setIndex] = React.useState(0);
   const addToBasket = () => {
     const item = { product: database[0].product, price: database[0].price };
     setBasket(currentBasket => [...currentBasket, item]);
@@ -11,6 +13,18 @@ const Product = () => {
   return (
     <div>
       <p>{database[0].product}</p>
+      <div style={{ width: "50vw", height: "50vh" }}>
+        <Gallery
+          index={index}
+          onRequestChange={i => {
+            setIndex(i);
+          }}
+        >
+          {images.map(img => (
+            <GalleryImage objectFit="contain" key={img.src} src={img.src} />
+          ))}
+        </Gallery>
+      </div>
       <p>£{database[0].price}</p>
       <p>{database[0].description}</p>
       <p>Quantity in Basket: {basket.length}</p>
